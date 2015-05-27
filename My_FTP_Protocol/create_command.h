9191102dd145168ea35bee00aa691f916f5f12f1
smd519 <smosharr@uwaterloo.ca>
2015-05-26 23:54:21 -0400

    fixing errors

--- My_FTP_Protocol/create_command.h
+++ My_FTP_Protocol/create_command.h
@@ -11,6 +11,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <basics.h>
 
 char create_command (char* user_input, char* arg, char* std_message);
 char send_command(int sockfd,char* fttp_message, int message_length);
