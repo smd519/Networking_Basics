31e270f265bc3c1ef9d635c2797306cb5017ad97
smd519 <smosharr@uwaterloo.ca>
2015-05-27 23:45:55 -0400

    Closing Data Socket after each use and openning a new one AND reading comtrol socket both before and after data-socket

--- My_FTP_Protocol/FTP_Client.c
+++ My_FTP_Protocol/FTP_Client.c
@@ -17,13 +17,13 @@
 #include <passive.h>
 #include <recv_ctrl_socket.h>
 #include <create_command.h>
+#include <create_pasv_dsocket.h>
 
 #define CTRLPORT "21"
 #define DATAPORT "20"
 
 int main(int argc, char *argv[]) {
 	if (argc==1) {
-		// If No IP address is entered, just lookuP the IP-address of the machine
 		printf ("please enter the server IP address\n");
 	}
 	char tIP[16]={0};
@@ -43,23 +43,32 @@ int main(int argc, char *argv[]) {
 	char flag=1;
 	char user_input[64];
 	char std_message[70];
-
 	//create_data_socket (sockfd);
 	while (flag){
 		printf("ftp> ");
 		memset(&user_input,0,64);
-		memset(&std_message,0,70);
-
 		fgets(user_input, sizeof(user_input), stdin);
 		printf("User input:%s\n",user_input);
+		if (strcmp(user_input, "\n")==0) {
+			continue;
+		}
 
-		status=create_command(user_input, NULL,std_message);
-		if (status!=0 && status!=2) {
+		memset(&std_message,0,70);
+		int command_status=create_command(user_input, NULL,std_message);
+		if (command_status!=0 && command_status!=2 && command_status!=3) {
 			return -1;
 			terminate_socket (sockfd);
 		}
-		if (status==2) {
+		if (command_status==2) {///exit,bye,quit
 			flag=0;
+		}
+
+		int data_sockfd=0;
+		if (command_status==3) {///if the command needs a data socket
+			data_sockfd=create_pasv_dsocket(sockfd);
+			if (data_sockfd==-1) {
+				return -1;
+			}
 		}
 
 		status=send_command(sockfd,std_message, strlen(std_message));
@@ -68,10 +77,26 @@ int main(int argc, char *argv[]) {
 			terminate_socket (sockfd);
 		}
 
+		if (command_status==3) {///if the command needs a data socket
+			status=recv_response(data_sockfd);
+			if (data_sockfd==-1) {
+				return -1;
+			}
+			status=recv_response(sockfd);
+			if (status!=0) {
+				return -1;
+				terminate_socket (sockfd);
+			}
+		}
+
 		status=recv_response(sockfd);
 		if (status!=0) {
 			return -1;
 			terminate_socket (sockfd);
+		}
+
+		if (command_status==3) {///Close Data Socket after use
+			terminate_socket (data_sockfd);
 		}
 
 	}
