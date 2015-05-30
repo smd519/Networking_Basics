1cf03c6704c870cf1bb96f8a1baaeb0b7977e697
smd519 <smosharr@uwaterloo.ca>
2015-05-30 06:48:10 -0400

    populating ip-header

---
+++ Syn_Scanner/ip_header.c
@@ -0,0 +1,44 @@
+/*
+ * ip_header.c
+ *
+ *  Created on: 2015-05-29
+ *      Author: sajjad
+ */
+#include <ip_header.h>
+
+
+char create_iph(char * tIP,struct iphdr *ip_header) {
+	struct in_addr  target;
+	memset(&target,0, sizeof(target));
+	int status=inet_aton(tIP, &target);
+	if (status==0) {
+		printf("address is not valid\n");
+		return -1;
+	}
+
+	struct in_addr source;
+	memset(&source,0, sizeof(source));
+	status=inet_aton("192.168.0.1",&source); //any valid ip address, lets do ip snoofing :D
+	if (status==0) {
+		printf("address is not valid\n");
+		return -1;
+	}
+
+	ip_header->ihl = 5;
+	ip_header->version = 4;
+	ip_header->tos = 72; //Immediate
+	ip_header->tot_len = sizeof (struct ip) + sizeof (struct tcphdr);
+	ip_header->id = htons((int)(rand()/(((double)RAND_MAX + 1)/14095))); //just random ID
+	ip_header->frag_off =0;
+	ip_header->ttl = 64;
+
+	ip_header->protocol = IPPROTO_TCP;
+	ip_header->saddr = source.s_addr;
+	ip_header->daddr = target.s_addr;
+
+	ip_header->check = 0;      //Set to 0 before calculating checksum
+	ip_header->check = cksum (ip_header, ip_header->tot_len);
+
+	return 0;
+}
+
---
+++ Syn_Scanner/ip_header.h
@@ -0,0 +1,21 @@
+/*
+ * ip_header.h
+ *
+ *  Created on: 2015-05-29
+ *      Author: sajjad
+ */
+
+#ifndef IP_HEADER_H_
+#define IP_HEADER_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <checksum.h>
+
+char create_iph(char * tIP,struct iphdr *ip_header);
+
+#endif /* IP_HEADER_H_ */
